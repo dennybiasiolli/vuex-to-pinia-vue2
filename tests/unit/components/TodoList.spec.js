3c944c2c76cffeeded86bc875c5bf36e141ce3c0
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-04-24 14:24:39 +0200

    TodoList: switching from vuex to pinia

--- tests/unit/components/TodoList.spec.js
+++ tests/unit/components/TodoList.spec.js
@@ -1,36 +1,33 @@
-import Vuex from 'vuex'
+import { PiniaVuePlugin } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import TodoList from '@/components/TodoList.vue'
-import { GET_TODO_LIST } from '@/store/modules/todo-types'
+import { useTodoStore } from '@/stores/todo'
 
 const localVue = createLocalVue()
-localVue.use(Vuex)
+localVue.use(PiniaVuePlugin)
 
 describe('TodoList.vue', () => {
-  let store;
-  let todoActions = {
-    [GET_TODO_LIST]: jest.fn(),
-  }
+  let pinia;
+  let todoStore;
   beforeEach(() => {
-    store = new Vuex.Store({
-      modules: {
+    pinia = createTestingPinia({
+      initialState: {
         todo: {
-          namespaced: true,
-          state: {
-            todoList: [
-              { id: 1, name: 'test', completed: true },
-              { id: 2, name: 'test2', completed: false },
-            ],
-            todoListLoading: false,
-            todoListLoadingError: null,
-          },
-          actions: todoActions,
+          todoList: [
+            { id: 1, name: 'test', completed: true },
+            { id: 2, name: 'test2', completed: false },
+          ],
+          todoListLoading: false,
+          todoListLoadingError: null,
         },
       },
     })
+    todoStore = useTodoStore()
   })
   test('should respect the snapshot', () => {
-    const wrapper = shallowMount(TodoList, { localVue, store })
+    const wrapper = shallowMount(TodoList, { localVue, pinia })
     expect(wrapper.element).toMatchSnapshot()
+    expect(todoStore.getTodoList).toHaveBeenCalledWith()
   })
 })
