9fc743ded9b78c227b7aa1d5a398977fd42f5f15
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-04-24 14:07:15 +0200

    pinia: creating todo store with tests

---
+++ tests/unit/stores/todo.spec.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import { defaultState, actions } from '@/stores/todo'
+
+jest.mock('axios', () => ({
+  get: jest.fn(),
+}))
+
+describe('pinia main store', () => {
+  describe('state', () => {
+    test('should be as expected', () => {
+      expect(defaultState).toEqual({
+        todoList: [],
+        todoListLoading: false,
+        todoListLoadingError: null,
+      })
+    })
+  })
+
+  describe('actions', () => {
+    test('getTodoList success', async () => {
+      const resolvedValue = { data: [{ id: 1, title: 'test' }, { id: 2, title: 'test2' }] }
+      axios.get.mockReset().mockResolvedValue(resolvedValue)
+      const state = {}
+      const params = { page: 1 }
+      const promise = actions.getTodoList.call(state, params)
+      expect(state.todoListLoading).toBe(true)
+      expect(state.todoListLoadingError).toBeNull()
+      expect(axios.get).toHaveBeenCalledWith('data/todoList.json', { params })
+      await promise;
+      expect(state.todoList).toEqual(resolvedValue.data)
+      expect(state.todoListLoading).toBe(false)
+    })
+    test('getTodoList failure', async () => {
+      const rejectedValue = 'foo'
+      axios.get.mockReset().mockRejectedValue(rejectedValue)
+      const state = {}
+      const params = { page: 1 }
+      const promise = actions.getTodoList.call(state, params)
+      expect(state.todoListLoading).toBe(true)
+      expect(state.todoListLoadingError).toBeNull()
+      expect(axios.get).toHaveBeenCalledWith('data/todoList.json', { params })
+      await promise;
+      expect(state.todoListLoadingError).toBe(rejectedValue)
+      expect(state.todoList).toEqual([])
+      expect(state.todoListLoading).toBe(false)
+    })
+  })
+})
